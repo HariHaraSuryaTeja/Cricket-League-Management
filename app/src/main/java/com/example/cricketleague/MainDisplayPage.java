2434a56c49bf1a9483fbd85f0c034e2fcd72e295
palanchi22 <palanchi22@gmail.com>
2020-05-14 19:02:02 -0400

    first commit

--- app/src/main/java/com/example/cricketleague/MainDisplayPage.java
+++ app/src/main/java/com/example/cricketleague/MainDisplayPage.java
@@ -1,8 +1,13 @@
 package com.example.cricketleague;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainDisplayPage extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainDisplayPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maindisplaypage);
+        BottomNavigationView bottomnav = findViewById(R.id.bottom_navigation);
+        bottomnav.setOnNavigationItemSelectedListener(navListener);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment =null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_account:
+                            selectedFragment = new AccountFragment();
+                            break;
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_news:
+                            selectedFragment = new NewsFragment();
+                            break;
+                        case R.id.nav_notification:
+                            selectedFragment = new NotificationFragment();
+                            break;
+                        case R.id.nav_matches:
+                            selectedFragment = new MatchesFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
