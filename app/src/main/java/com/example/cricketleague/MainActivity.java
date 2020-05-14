bf8e743409fab2f9386408e6abec1cd4b2a0ea6a
vedantherukulla <vedanth19@gmail.com>
2020-05-13 23:23:44 -0400

    splash and bottom navigation added

---
+++ app/src/main/java/com/example/cricketleague/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.cricketleague;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+private static int time_out=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i= new Intent(MainActivity.this, MainDisplayPage.class);
+                startActivity(i);
+            }
+        },time_out);
+
+    }
+}
